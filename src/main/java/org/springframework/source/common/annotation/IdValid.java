4c7ee7feea6fbe944009e833f0c902c530f9528b
ksg412 <ksg412@gmail.com>
2016-06-09 14:58:16 +0900

    대소문자 문제

---
+++ src/main/java/org/springframework/source/common/annotation/IdValid.java
@@ -0,0 +1,21 @@
+package org.springframework.source.common.annotation;
+
+import org.springframework.source.common.validator.IdOverlabValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.*;
+
+/**
+ * Created by sg on 2016-06-08.
+ */
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy={IdOverlabValidator.class})
+public @interface IdValid {
+
+    String message() default "{validator.idoverlab.check}";
+    Class<?>[] groups() default {};
+    Class<?>[] payload() default {};
+
+}
