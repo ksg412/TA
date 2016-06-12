3fdcfc472de0ac72892fc8318fc011e7ab3a2d34
ksg412 <ksg412@gmail.com>
2016-06-12 15:20:49 +0900

    사용자관리 , 마이페이지 구현완료

---
+++ src/main/java/org/springframework/source/userManagement/UserManagementController.java
@@ -0,0 +1,43 @@
+package org.springframework.source.userManagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.source.common.util.CommonUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by sg on 2016-06-09.
+ */
+@Controller
+@RequestMapping("/userManagement/*")
+public class UserManagementController {
+
+    @Autowired
+    private UserManagementService userManagementService;
+
+    @RequestMapping(value = "userManagementView.do")
+    public String userManagementView() throws Exception{
+
+        return "/jsp/userManagement/userManagement";
+    }
+
+    @ResponseBody
+    @RequestMapping(value="selectUserList.do")
+    public Object selectUserList(@RequestParam Map commandMap) throws Exception{
+        CommonUtil.mapFormDataToNomalData(commandMap);
+        List dataList = userManagementService.selectUserList(commandMap);
+        int dataCount = userManagementService.selectUserCount(commandMap);
+        Map returnMap = new HashMap();
+        returnMap.put("data",dataList);
+        returnMap.put("draw",commandMap.get("draw"));
+        returnMap.put("recordsTotal",dataCount);
+        returnMap.put("recordsFiltered",dataCount);
+        return returnMap;
+    }
+}
