d687682e42fb38c6018563db1142890d2dc672ac
ksg412 <ksg412@gmail.com>
2016-06-12 15:44:25 +0900

    대소문자수정용

--- src/main/java/org/springframework/source/mypage/MyPageService.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.source.myPage;
-
-/**
- * Created by sg on 2016-06-12.
- */
-public interface MyPageService {
-    MyPageVo selectMyPageView(String id) throws Exception;
-    void updateMyPageInfo(MyPageVo myPageVo) throws Exception;
-}
